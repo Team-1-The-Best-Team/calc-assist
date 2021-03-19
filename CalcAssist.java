a173c072e72447a54abe68a9654d71d1d65b5054
Juan Mejia <Jmeji099@fiu.edu>
2021-03-19 13:45:03 -0400

    File Structure For Calc-Assist

---
+++ CalcAssist.java
@@ -0,0 +1,11 @@
+/**
+ * CEN4010 Project 
+ * Calc Assist is a Calulator Assistance tool for Elementary School Students and Teachers 
+ * for basic calculator use and learning purposes.
+ */
+public class CalcAssist{
+
+    public static void main(String[] args){
+        
+    }
+}
